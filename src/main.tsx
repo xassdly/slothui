9b7d7219ec6448de1427c42d04fb10e4856da0ad
xassdly <xassdly@gmail.com>
2025-08-04 16:17:07 +0200

    deploy x2

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/flown'>
       <App />
     </BrowserRouter>
   </StrictMode>,
